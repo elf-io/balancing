4c223b8ed5db0d83f0ef305373f5ca3f22963b8f
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2024-10-14 15:47:27 +0800

    new map
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- pkg/ebpfEvent/event.go
+++ pkg/ebpfEvent/event.go
@@ -2,12 +2,13 @@ package ebpfEvent
 
 import (
 	"fmt"
+	"time"
+
 	"github.com/elf-io/balancing/pkg/ebpf"
 	"github.com/elf-io/balancing/pkg/ebpfWriter"
 	"github.com/elf-io/balancing/pkg/podId"
 	"github.com/elf-io/balancing/pkg/types"
 	"go.uber.org/zap"
-	"time"
 )
 
 type ebpfEventStruct struct {
@@ -30,6 +31,29 @@ func NewEbpfEvent(l *zap.Logger, ebpfHandler ebpf.EbpfProgram, writer ebpfWriter
 	}
 }
 
+// 定义一个结构体来存储事件信息
+type EventInfo struct {
+	ClientPodName string
+	Namespace     string
+	PodUuid       string
+	ContainerId   string
+	HostClient    bool
+	NodeName      string
+	IsIpv4        int
+	IsSuccess     int
+	DestIp        string
+	DestPort      int
+	NatIp         string
+	NatPort       int
+	Pid           int
+	Failure       string
+	TimeStamp     string
+	ServiceId     uint32
+	PolicyName    string
+	NatType       string
+	NatMode       string
+}
+
 func (s *ebpfEventStruct) WatchEbpfEvent(stopWatch chan struct{}) {
 	go func() {
 		eventCh := s.ebpfHandler.GetMapDataEvent()
@@ -45,58 +69,70 @@ func (s *ebpfEventStruct) WatchEbpfEvent(stopWatch chan struct{}) {
 				}
 
 				s.l.Sugar().Debugf("received an ebpf event: %s ", event)
-				var eventStr string
+
+				eventInfo := EventInfo{
+					NodeName:  types.AgentConfig.LocalNodeName,
+					IsIpv4:    event.IsIpv4,
+					IsSuccess: event.IsSuccess,
+					Pid:       event.Pid,
+					Failure:   ebpf.GetFailureStr(event.FailureCode),
+					TimeStamp: time.Now().UTC().Format("2006-01-02T15:04:05Z"),
+					ServiceId: event.SvcId,
+					NatType:   ebpf.GetNatTypeStr(event.NatType),
+					NatMode:   ebpf.GetNatModeStr(event.NatMode),
+				}
 
 				podName, namespace, containerId, podUuid, hostFlag, err := podId.PodIdHander.LookupPodByPid(event.Pid)
 				if err != nil {
 					s.l.Sugar().Errorf("failed to get podName for pid %d: %v", event.Pid, err)
-					// container application , but miss pod name
-					eventStr += fmt.Sprintf("ClientPodName=unknown, Namespace=unknown, podUuid=unknown, ContainerId=unknown, HostClient=false, ")
+					eventInfo.ClientPodName = "unknown"
+					eventInfo.Namespace = "unknown"
+					eventInfo.PodUuid = "unknown"
+					eventInfo.ContainerId = "unknown"
+					eventInfo.HostClient = false
 				} else {
 					if hostFlag {
-						eventStr += fmt.Sprintf("ClientPodName=, Namespace=, PodUuid=, ContainerId=, HostClient=true, ")
+						eventInfo.HostClient = true
 					} else {
 						if len(podName) > 0 {
-							// k8s pod
-							eventStr += fmt.Sprintf("ClientPodName=%s, Namespace=%s, PodUuid=, ContainerId=, HostClient=false, ", podName, namespace)
+							eventInfo.ClientPodName = podName
+							eventInfo.Namespace = namespace
+							eventInfo.HostClient = false
 						} else {
-							// just a container
-							eventStr += fmt.Sprintf("ClientPodName=, Namespace=, PodUuid=%s, ContainerId=%s, HostClient=false, ", podUuid, containerId)
+							eventInfo.PodUuid = podUuid
+							eventInfo.ContainerId = containerId
+							eventInfo.HostClient = false
 						}
 					}
 				}
-				eventStr += fmt.Sprintf("NodeName=%s, ", types.AgentConfig.LocalNodeName)
-				eventStr += fmt.Sprintf("IsIpv4=%d, IsSuccess=%d, ", event.IsIpv4, event.IsSuccess)
-				if event.IsIpv4 != 0 {
-					eventStr += fmt.Sprintf("DestIp=%s, DestPort=%d, NatIp=%s, NatPort=%d, ",
-						ebpf.GetIpStr(event.OriginalDestV4Ip), event.OriginalDestPort, ebpf.GetIpStr(event.NatV4Ip), event.NatPort)
-				} else {
-					eventStr += fmt.Sprintf("DestIp=%s, DestPort=%d, NatIp=%s, NatPort=%d, ",
-						ebpf.GetIpv6Str(event.OriginalDestV6ipHigh, event.OriginalDestV6ipLow), event.OriginalDestPort, ebpf.GetIpv6Str(event.NatV6ipHigh, event.NatV6ipLow), event.NatPort)
-				}
-				eventStr += fmt.Sprintf("Pid=%d, Failure=%s, ", event.Pid, ebpf.GetFailureStr(event.FailureCode))
-				stamp := time.Now().UTC()
-				eventStr += fmt.Sprintf("TimeStamp=%s ", stamp.Format("2006-01-02T15:04:05Z"))
 
-				// print the related service/localRedirect/balancing
-				eventStr += fmt.Sprintf("serviceId=%d ", event.SvcId)
-				eventStr += fmt.Sprintf("NatType=%s, NatMode=%s, ", ebpf.GetNatTypeStr(event.NatType), ebpf.GetNatModeStr(event.NatMode))
+				if event.IsIpv4 != 0 {
+					eventInfo.DestIp = ebpf.GetIpStr(event.OriginalDestV4Ip)
+					eventInfo.DestPort = event.OriginalDestPort
+					eventInfo.NatIp = ebpf.GetIpStr(event.NatV4Ip)
+					eventInfo.NatPort = event.NatPort
+				} else {
+					eventInfo.DestIp = ebpf.GetIpv6Str(event.OriginalDestV6ipHigh, event.OriginalDestV6ipLow)
+					eventInfo.DestPort = event.OriginalDestPort
+					eventInfo.NatIp = ebpf.GetIpv6Str(event.NatV6ipHigh, event.NatV6ipLow)
+					eventInfo.NatPort = event.NatPort
+				}
+
 				namespace, name, err := s.writer.GetPolicyBySvcId(event.NatType, event.SvcId)
 				if err != nil {
 					s.l.Sugar().Errorf("failed to find policy for ebpf event with natMode=%s and svcId=%d : %v ", ebpf.GetNatTypeStr(event.NatType), event.SvcId, err)
-					eventStr += fmt.Sprintf("policyName= ")
+					eventInfo.PolicyName = ""
 				} else {
 					switch event.NatType {
 					case ebpf.NAT_TYPE_SERVICE:
-						eventStr += fmt.Sprintf("policyName=%s/%s ", namespace, name)
+						eventInfo.PolicyName = fmt.Sprintf("%s/%s", namespace, name)
 					default:
-						eventStr += fmt.Sprintf("policyName=%s ", name)
+						eventInfo.PolicyName = name
 					}
 				}
 
-				s.l.Sugar().Infof("formatted ebpf event: %s", eventStr)
+				s.l.Sugar().Infof("formatted ebpf event: %+v", eventInfo)
 			}
 		}
 	}()
-
 }
