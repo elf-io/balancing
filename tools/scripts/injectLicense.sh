64fe03a6da72f041ee4debcb4d51aa8644d22dbe
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2024-10-28 11:30:08 +0800

    asd
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- tools/scripts/injectLicense.sh
+++ tools/scripts/injectLicense.sh
@@ -8,10 +8,12 @@ HEADER_TITLE="// Copyright $(date +%Y) Authors of elf-io\n// SPDX-License-Identi
 
 GO_FILE_LIST=$( grep -v "${HEADER_TITLE}" --include \*.go  . -RHn --colour -l  --exclude-dir={vendor,charts,output} )
 
+echo -e "$HEADER_TITLE" > /tmp/.header
+
 for FILE in $GO_FILE_LIST ; do
    echo "inject license header to $FILE"
-   sed -i '1i\
-   '$HEADER_TITLE'
-   ' $FILE
+   while read LINE ; do
+       # 使用空的备份后缀以确保兼容性
+       sed -i '' '1i  '$LINE'' $FILE
+   done < /tmp/.header
 done
-
