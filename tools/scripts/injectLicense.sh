db3547dbd7912a0089ee69f06dc37ed2d5961626
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2024-10-28 11:24:02 +0800

    asd
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- tools/scripts/injectLicense.sh
+++ tools/scripts/injectLicense.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+CURRENT_DIR_PATH=$(cd `dirname $0`; pwd)
+PROJECT_ROOT_PATH="${CURRENT_DIR_PATH}/../.."
+cd ${PROJECT_ROOT_PATH}
+
 HEADER_TITLE="// Copyright $(date +%Y) Authors of elf-io\n// SPDX-License-Identifier: Apache-2.0"
 
 GO_FILE_LIST=$( grep -v "${HEADER_TITLE}" --include \*.go  . -RHn --colour -l  --exclude-dir={vendor,charts,output} )
