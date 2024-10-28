1e2e636513bd4b432fe395f6734b8db40e56576b
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2024-10-28 12:24:54 +0800

    d
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

---
+++ tools/scripts/injectLicense.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+CURRENT_DIR_PATH=$(cd `dirname $0`; pwd)
+PROJECT_ROOT_PATH="${CURRENT_DIR_PATH}/../.."
+cd ${PROJECT_ROOT_PATH}
+
+HEADER_TITLE="Copyright $(date +%Y) Authors of elf-io"
+SPDX_IDENTIFIER="SPDX-License-Identifier: Apache-2.0"
+
+GO_FILE_LIST=$(find . -type f -name "*.go" ! -path "./vendor/*" ! -path "./charts/*" ! -path "./output/*")
+for FILE in $GO_FILE_LIST ; do
+   if ! grep "SPDX-License-Identifier" $FILE &>/dev/null ; then
+       echo "inject license header to $FILE"
+       echo -e "// $HEADER_TITLE\n// $SPDX_IDENTIFIER\n$(cat $FILE)" > $FILE
+       sed -i '1 i \\\\\ '${SPDX_IDENTIFIER}'' $FILE
+       sed -i '1 i \\\\\ '${HEADER_TITLE}'' $FILE
+   fi
+done
+
+SH_FILE_LIST=$(find . -type f -name "*.sh" ! -path "./vendor/*" ! -path "./charts/*" ! -path "./output/*")
+for FILE in ${SH_FILE_LIST} ; do
+   if ! grep "SPDX-License-Identifier" $FILE &>/dev/null ; then
+       echo "inject license header to $FILE"
+       sed -i '1 a \# '"${SPDX_IDENTIFIER}"'' $FILE
+       sed -i '1 a \# '"${HEADER_TITLE}"'' $FILE
+   fi
+done
+
